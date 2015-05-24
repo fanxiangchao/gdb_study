1efca035416da958a0dfb473f7106a09fb89774f
fanxiangchao <coderspike@163.com>
2015-05-24 15:39:20 +0800

    swapflaw.c

---
+++ swapflaw.c
@@ -0,0 +1,26 @@
+/* page 57
+swapflaw.c
+a flawed function that swaps two integers
+*/
+#include <stdio.h>
+void swap(int a,int b);
+
+int main(void)
+{
+  int i = 4;
+  int j = 6;
+  
+  printf("i: %d, j: %d\n", i, j);
+  swap(&i, &j);
+  printf("i: %d, j: %d\n", i, j);
+  
+  return 0;
+}
+
+void swap(int a,int b)
+{
+  int c = a;
+  a = b;
+  b = c;
+}
+
